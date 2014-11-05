d77b460f4ff3476bc5831c0319be084e56ee5831
Shiul93 <lfllamas93@gmail.com>
2014-11-05 19:54:39 +0100

    Añadido el log con conversaciones, por las risas y la funcionalidad de irc

--- pybot.py
+++ pybot.py
@@ -16,6 +16,94 @@ import irc
 f = open('clever.log','a')
 stdin = sys.stdin
 
+import socket
+
+class IRCClient:
+    socket = None
+    connected = False
+    nickname = 'ShiulTroller'
+    channels = ['#ProyectoMagallanes']
+    server='irc.Mibbit.Net'
+    
+    def __init__(self,server,channel,nickname):
+        self.socket = socket.socket()
+        self.socket.connect((server, 6667))
+        self.send("NICK %s" % self.nickname)
+        self.send("USER %(nick)s %(nick)s %(nick)s :%(nick)s" % {'nick':self.nickname})
+        cleverbot_client_one = cleverbot.Cleverbot()
+        exit=True
+        while exit==True:
+            buf = self.socket.recv(4096)
+            lines = buf.split("\n")
+            for data in lines:
+                data = str(data).strip()
+
+                if data == '':
+                    continue
+                print "I<", data
+
+                # server ping/pong?
+                if data.find('PING') != -1:
+                    n = data.split(':')[1]
+                    self.send('PONG :' + n)
+                    if self.connected == False:
+                        self.perform()
+                        self.connected = True
+
+                args = data.split(None, 3)
+                if len(args) != 4:
+                    continue
+                ctx = {}
+                ctx['sender'] = args[0][1:]
+                ctx['type']   = args[1]
+                ctx['target'] = args[2]
+                ctx['msg']    = args[3][1:]
+
+                # whom to reply?
+                target = ctx['target']
+                if ctx['target'] == self.nickname:
+                    target = ctx['sender'].split("!")[0]
+
+                # some basic commands
+                if ctx['msg'] == '!help':
+                    self.say('available commands: !help', target)
+
+                # directed to the bot?
+                if ctx['type'] == 'PRIVMSG' and (ctx['msg'].lower()[0:len(self.nickname)] == self.nickname.lower() or ctx['target'] == self.nickname):
+                    # something is speaking to the bot
+                    query = ctx['msg']
+                    if ctx['target'] != self.nickname:
+                        query = query[len(self.nickname):]
+                        query = query.lstrip(':,;. ')
+                    # do something intelligent here, like query a chatterbot
+                    print 'someone spoke to us: ', query
+                    if query== 'pirate':
+                        exit=False
+                        self.say('Adios mundo cruel', target)
+                        
+                    else:
+                        response = cleverbot_client_one.ask(query)
+                        self.say(response, target)
+
+    def send(self, msg):
+        print "I>",msg
+        self.socket.send(msg+"\r\n")
+
+    def say(self, msg, to):
+        self.send("PRIVMSG %s :%s" % (to, msg))
+
+    def perform(self):
+        #self.send("PRIVMSG R : Register <>")
+        self.send("PRIVMSG R : Login <>")
+        self.send("MODE %s +x" % self.nickname)
+        for c in self.channels:
+            self.send("JOIN %s" % c)
+            # say hello to every channel
+            self.say('Hola a todos menos a JavierNH y a Bot, que son unos estirados', c)
+
+
+
+
 def conversation():
     # instantiate two Cleverbot objects
     
@@ -49,9 +137,12 @@ def conversation():
         #print '##Answer= '+answer+' ##'
 
 def ircBot():
-    print
-    ircRoboto= irc.bot.SingleServerIRCBot()
- 
+    nickname = 'ShiulTroller'
+    channels = '#ProyectoMagallanes'
+    server='irc.Mibbit.Net'
+    
+    IRCClient(server,channels,nickname)
+
 def testFunct():
     testStr= stdin.readline()
     print testStr
